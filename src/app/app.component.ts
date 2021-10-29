81b323ae79d57b4cf408f0e6ad399f6616b7b213
vinceeeent <vincent.ontuca@evsu.edu.ph>
2021-10-29 10:04:47 +0800

    added router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'copy';
+  title = 'covid-19-Vaccine Information System';
 }
